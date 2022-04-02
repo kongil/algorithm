da3cc0f8e890a6d5e1125683da557e4ec48beccc
kongil <o_o0122@naver.com>
2022-04-02 18:53:08 +0900

    선택 정렬

--- src/Ex6_1.java
+++ src/Ex6_1.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
+// 선택 정렬
 public class Ex6_1 {
 
     public static int arr[] = {7,5,9,0,3,1,6,2,4,8};
 
     public static void main(String[] args) {
-        // 선택 정렬
         int n = arr.length;
         for (int i = 0; i < arr.length; i++) {
             int min_index = i;
