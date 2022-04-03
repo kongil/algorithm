2eaf8e36ccb2c157d58b23a650ea3dc06d63e573
kongil <o_o0122@naver.com>
2022-04-03 23:02:33 +0900

    그래프 리스트 형태

--- src/Ex5_7.java
+++ src/Ex5_7.java
@@ -1,11 +1,11 @@
 
 import java.util.*;
 
-class Node {
+class Node2 {
     int index;
     int distance;
 
-    public Node(int index, int distance) {
+    public Node2(int index, int distance) {
         this.index = index;
         this.distance = distance;
     }
@@ -16,19 +16,19 @@ class Node {
 }
 
 public class Ex5_7 {
-    public static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
+    public static ArrayList<ArrayList<Node2>> graph = new ArrayList<ArrayList<Node2>>();
 
     public static void main(String[] args) {
         for (int i = 0; i < 3; i++) {
-            graph.add(new ArrayList<Node>());
+            graph.add(new ArrayList<Node2>());
         }
 
-        graph.get(0).add(new Node(1, 7));
-        graph.get(0).add(new Node(2, 5));
+        graph.get(0).add(new Node2(1, 7));
+        graph.get(0).add(new Node2(2, 5));
 
-        graph.get(1).add(new Node(0, 7));
+        graph.get(1).add(new Node2(0, 7));
 
-        graph.get(2).add(new Node(0, 5));
+        graph.get(2).add(new Node2(0, 5));
 
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < graph.get(i).size(); j++) {
