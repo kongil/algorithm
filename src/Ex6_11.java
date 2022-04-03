2540edf420998e4b1daee39490977c94caa445c3
kongil <o_o0122@naver.com>
2022-04-03 23:04:52 +0900

    키값을 이용한 정렬 실전(성적이 낮은 순서로 학생 출력하기)

---
+++ src/Ex6_11.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Student implements Comparable<Student>{
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    @Override
+    public int compareTo(Student o) {
+        if (this.score > o.score)
+            return 1;
+        else
+            return -1;
+    }
+}
+
+public class Ex6_11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Student> students = new ArrayList<Student>();
+
+        for (int i = 0; i < n; i++) {
+            String name = sc.next();
+            int score = sc.nextInt();
+            students.add(new Student(name, score));
+        }
+
+        Collections.sort(students);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(students.get(i).getName() + " ");
+        }
+    }
+}
