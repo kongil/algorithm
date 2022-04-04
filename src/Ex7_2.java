91a670fee9e2457cf42c34c22c43615f0e119ada
kongil <o_o0122@naver.com>
2022-04-05 01:03:16 +0900

    이진 탐색(재귀 구현)

---
+++ src/Ex7_2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+// 재귀를 이용한 이진 탐색 구현
+public class Ex7_2 {
+    public static int binarySearch(int arr[], int target, int start, int end) {
+        if (start > end) return -1;
+        int mid = (start + end) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] < target) {
+            return binarySearch(arr, target, mid + 1, end);
+        }
+        else {
+            return binarySearch(arr, target, start, mid - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 원소의 개수(n)와 찾고자 하는 값(target)을 입력받기
+        int n = sc.nextInt();
+        int target = sc.nextInt();
+
+        // 전체 원소 받기
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i ++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int result = binarySearch(arr, target, 0, n - 1);
+        if (result == -1)
+            System.out.println("원소가 존재하지 않습니다.");
+        else
+            System.out.println(result+1);
+
+    }
+}
