1d134c22735fa56a6aa339cbf3d52fba29fe2802
kongil <o_o0122@naver.com>
2022-07-08 23:37:14 +0900

    선택정렬

--- sort/src/SelectionSort.java
+++ sort/src/SelectionSort.java
@@ -5,6 +5,7 @@ public class SelectionSort {
     public static void main(String[] args) {
         int[] arr = {6,3,1,2,3,5,7,9};
         int n = arr.length;
+        // 가장 작은 데이터를 선택해 맨 앞의 데이터와 바꾸고 그 다음 작은 데이터를 선택해 2번째 데이터와 바꾸기를 반복한다.
         for (int i = 0; i < n; i++) {
             int minIdx = i;
             for (int j = i+1; j < n; j++) {
