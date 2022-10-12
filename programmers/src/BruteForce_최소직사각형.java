5938f4cb1ea48952b5712cfad4d81914bb513578
kongil <o_o0122@naver.com>
2022-10-13 05:53:04 +0900

    최소직사각형 (완전탐색)

---
+++ programmers/src/BruteForce_최소직사각형.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class BruteForce_최소직사각형 {
+    static class Solution {
+        public int solution(int[][] sizes) {
+            int w = 0;
+            int h = 0;
+            for (int[] size : sizes) {
+                int tmpW = size[0];
+                int tmpH = size[1];
+                if (tmpW < tmpH) {
+                    int tmp = tmpH;
+                    tmpH = tmpW;
+                    tmpW = tmp;
+                }
+                w = Math.max(w, tmpW);
+                h = Math.max(h, tmpH);
+            }
+            return w * h;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "()()";
+        boolean isRight = solution.solution(s);
+        System.out.println("isRight = " + isRight);
+    }
+}
