5cc9ff6ae599eaa78c5b47ef6d3bc579b7c10e40
kongil <o_o0122@naver.com>
2022-10-27 23:52:18 +0900

    조이스틱 (재귀) 왜 그리디지?

--- programmers/src/Greedy_조이스틱.java
+++ programmers/src/Greedy_조이스틱.java
@@ -6,23 +6,8 @@ public class Greedy_조이스틱 {
     static class Solution {
         boolean[] visited;
         int[] cntArr;
-        static class Stick {
-            int pos;
-            int cnt;
+        int minCnt = 10000000;
 
-            public Stick(int pos, int cnt) {
-                this.pos = pos;
-                this.cnt = cnt;
-            }
-
-            public int getPos() {
-                return pos;
-            }
-
-            public int getCnt() {
-                return cnt;
-            }
-        }
         public int[] makeCntArr(String name) {
             int len = name.length();
             char[] nameArr = name.toCharArray();
@@ -35,38 +20,44 @@ public class Greedy_조이스틱 {
             }
             return cntArr;
         }
-        public void bfs(int now) {
-            Queue<Stick> q = new LinkedList<>();
-            visited[now] = true;
-            q.add(new Stick(now, cntArr[now]));
-
-            while (!q.isEmpty()) {
-                Stick s = q.poll();
-                now = s.getPos();
-                int cnt = s.getCnt();
-                System.out.println(now + " : " + cnt);
-                int left = now-1;
-                if (left < 0) left += visited.length;
-                int right = (now + 1) % visited.length;
-                if (!visited[left]) {
-                    visited[left] = true;
-                    q.add(new Stick(left, cnt + 1 + cntArr[left]));
-                }
-                if (!visited[right]) {
-                    visited[right] = true;
-                    q.add(new Stick(left, cnt + 1 + cntArr[right]));
-                }
+        public int dfs(int now) {
+            int cnt = cntArr[now];
+            int len = visited.length;
+            int left = now - 1;
+            if(left < 0) left += len;
+            while ((visited[left] || cntArr[left] == 0) && left != now) {
+                left = left - 1;
+                if(left < 0) left+= len;
             }
+            int right = (now+1) % len;
+            while ((visited[right] || cntArr[right] == 0) && right != now) {
+                right = (right + 1) % len;
+            }
+
+            int leftCnt = minCnt, rightCnt = minCnt;
+            if (left != now && !visited[left]) {
+                visited[left] = true;
+                leftCnt = cnt + dfs(left) + Math.min(Math.abs(now - left), len - Math.abs(now - left));
+                visited[left] = false;
+            }
+            if (right != now && !visited[right]) {
+                visited[right] = true;
+                rightCnt = cnt + dfs(right) + Math.min(Math.abs(now - right), len - Math.abs(now - right));
+                visited[right] = false;
+            }
+
+            cnt = Math.min(leftCnt, rightCnt);
+            if (cnt == minCnt) cnt = cntArr[now];
+
+            return cnt;
         }
         public int solution(String name) {
             int answer = 0;
             cntArr = makeCntArr(name);
             int len = cntArr.length;
             visited = new boolean[len];
-            for (int i = 0; i < len; i++) {
-                System.out.println(cntArr[i]);
-            }
-            bfs(0);
+            visited[0] = true;
+            answer = dfs(0);
             return answer;
         }
     }
