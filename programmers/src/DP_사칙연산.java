7f30c8e7046551b61e3f44661963ad225fee0290
kongil <o_o0122@naver.com>
2022-11-09 23:58:49 +0900

    사칙연산(dp - top-down : 틀린 풀이)

--- programmers/src/DP_사칙연산.java
+++ programmers/src/DP_사칙연산.java
@@ -7,15 +7,14 @@ public class DP_사칙연산 {
         public int solution(String arr[]) {
             int answer = -1;
             int n = arr.length;
-            int[][] dp = new int[n][2];
+            int[][] dp = new int[200][2];
 
+            dp[n-1][0] = Integer.valueOf(arr[n-1]);
+            dp[n-1][1] = Integer.valueOf(arr[n-1]);
             for (int i = n-1; i >= 0; i--) {
-                if (i == n-2) {
-                    continue;
-                }
-                else if (arr[i].equals("-")) {
-                    dp[i - 1][0] = Math.min(Integer.valueOf(arr[i - 1]) - dp[i + 1][1], Integer.valueOf(arr[i - 1]) - Integer.valueOf(arr[i+1]) + dp[i + 3][0]);
-                    dp[i - 1][1] = Math.max(Integer.valueOf(arr[i - 1]) - dp[i + 1][0], Integer.valueOf(arr[i - 1]) - Integer.valueOf(arr[i+1]) + dp[i + 3][1]);
+                if (arr[i].equals("-")) {
+                    dp[i-1][0] = Math.min(Integer.valueOf(arr[i-1]) - dp[i+1][1], Integer.valueOf(arr[i-1]) - Integer.valueOf(arr[i+1]) + dp[i+3][0]);
+                    dp[i-1][1] = Math.max(Integer.valueOf(arr[i-1]) - dp[i+1][0], Integer.valueOf(arr[i-1]) - Integer.valueOf(arr[i+1]) + dp[i+3][1]);
                 }
                 else if (arr[i].equals("+")) {
                     dp[i-1][0] = dp[i+1][0] + Integer.parseInt(arr[i - 1]);
@@ -26,6 +25,9 @@ public class DP_사칙연산 {
             for (int i = 0; i < n; i++) {
                 System.out.println(dp[i][0] + " : " + dp[i][1]);
             }
+
+            answer = dp[0][1];
+
             return answer;
         }
     }
