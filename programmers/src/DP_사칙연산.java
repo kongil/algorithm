ea6e961cc94ed94d68c81b3ff98bd87b1c97df08
kongil <o_o0122@naver.com>
2022-11-10 23:45:46 +0900

    사칙연산(dp - top-down : 틀린 풀이 -> 점화식부터 다시 세울것)

--- programmers/src/DP_사칙연산.java
+++ programmers/src/DP_사칙연산.java
@@ -5,20 +5,21 @@ import java.util.List;
 public class DP_사칙연산 {
     static class Solution {
         public int solution(String arr[]) {
+            int MIN = 0, MAX = 1;
             int answer = -1;
             int n = arr.length;
             int[][] dp = new int[200][2];
 
-            dp[n-1][0] = Integer.valueOf(arr[n-1]);
-            dp[n-1][1] = Integer.valueOf(arr[n-1]);
+            dp[n-1][MIN] = Integer.valueOf(arr[n-1]);
+            dp[n-1][MAX] = Integer.valueOf(arr[n-1]);
             for (int i = n-1; i >= 0; i--) {
                 if (arr[i].equals("-")) {
-                    dp[i-1][0] = Math.min(Integer.valueOf(arr[i-1]) - dp[i+1][1], Integer.valueOf(arr[i-1]) - Integer.valueOf(arr[i+1]) + dp[i+3][0]);
-                    dp[i-1][1] = Math.max(Integer.valueOf(arr[i-1]) - dp[i+1][0], Integer.valueOf(arr[i-1]) - Integer.valueOf(arr[i+1]) + dp[i+3][1]);
+                    dp[i-1][MIN] = Math.min(Integer.valueOf(arr[i-1]) - dp[i+1][MAX], Integer.valueOf(arr[i-1]) - Integer.valueOf(arr[i+1]) + dp[i+3][MIN]);
+                    dp[i-1][MAX] = Math.max(Integer.valueOf(arr[i-1]) - dp[i+1][MIN], Integer.valueOf(arr[i-1]) - Integer.valueOf(arr[i+1]) + dp[i+3][MAX]);
                 }
                 else if (arr[i].equals("+")) {
-                    dp[i-1][0] = dp[i+1][0] + Integer.parseInt(arr[i - 1]);
-                    dp[i-1][1] = dp[i+1][1] + Integer.parseInt(arr[i - 1]);
+                    dp[i-1][MIN] = dp[i+1][MIN] + Integer.parseInt(arr[i - 1]);
+                    dp[i-1][MAX] = dp[i+1][MAX] + Integer.parseInt(arr[i - 1]);
                 }
             }
 
