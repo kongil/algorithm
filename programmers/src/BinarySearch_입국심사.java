42428151cd9f161e8319b4bbc8a188391a0773bc
kongil <o_o0122@naver.com>
2022-12-09 23:45:00 +0900

    입국심사(이진탐색 - 파라메트릭 서치)

--- programmers/src/BinarySearch_입국심사.java
+++ programmers/src/BinarySearch_입국심사.java
@@ -2,14 +2,29 @@ import java.util.Arrays;
 
 public class BinarySearch_입국심사 {
     static class Solution {
-        public long countPerOneMan(long time, )
+        public long countPerOneMan(long totalTime, int[] times) {
+            long cnt = 0;
+            for (int time : times) {
+                cnt += totalTime / (long)time;
+            }
+            return cnt;
+        }
+
         public long solution(int n, int[] times) {
             long answer = 0;
             Arrays.sort(times);
-            long left = n * times[0];
-            long right = n * times[times.length - 1];
+            long left = 0;
+            long right = (long)n * times[times.length - 1];
             while (left < right) {
-
+                long mid = (left + right) / 2;
+                long cnt = countPerOneMan(mid, times);
+                if (cnt >= n) {
+                    right = mid;
+                    answer = mid;
+                }
+                else {
+                    left = mid+1;
+                }
             }
             return answer;
         }
