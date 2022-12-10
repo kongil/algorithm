66b4250bad3f1087aba277e7d18fec4fa0d518b0
kongil <o_o0122@naver.com>
2022-12-10 12:59:00 +0900

    징검다리(template)

---
+++ programmers/src/BinarySearch_징검다리.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class BinarySearch_징검다리 {
+    static class Solution {
+        public long countPerOneMan(long totalTime, int[] times) {
+            long cnt = 0;
+            for (int time : times) {
+                cnt += totalTime / (long)time;
+            }
+            return cnt;
+        }
+
+        public long solution(int n, int[] times) {
+            long answer = 0;
+            Arrays.sort(times);
+            long left = 0;
+            long right = (long)n * times[times.length - 1];
+            while (left < right) {
+                long mid = (left + right) / 2;
+                long cnt = countPerOneMan(mid, times);
+                if (cnt >= n) {
+                    right = mid;
+                    answer = mid;
+                }
+                else {
+                    left = mid+1;
+                }
+            }
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int n = 6;
+        int[] times = {7, 10};
+
+        long answer = solution.solution(n, times);
+        System.out.println("answer = " + answer);
+
+    }
+}
