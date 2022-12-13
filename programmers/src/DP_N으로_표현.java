4eac207e5cd5699178a3de450742d497475cf918
kongil <o_o0122@naver.com>
2022-12-13 23:24:23 +0900

    N으로 표현(dp)

--- programmers/src/DP_N으로_표현.java
+++ programmers/src/DP_N으로_표현.java
@@ -1,8 +1,41 @@
+import java.util.*;
+
 public class DP_N으로_표현 {
     static class Solution {
         public int solution(int N, int number) {
-            int answer = 0;
-            return answer;
+            List<Set<Integer>> setList = new ArrayList<>();
+            setList.add(new HashSet<>());
+            for (int i = 1; i <= 8; i++) {
+                String s = "";
+                for (int j = 1; j <= i; j++) {
+                    s += String.valueOf(N);
+                }
+                setList.add(new HashSet<>());
+                setList.get(i).add(Integer.valueOf(s));
+            }
+            for (int i = 1; i <= 8; i++) {
+                for (int j = 1; j < i; j++) {
+                    Set<Integer> setA = setList.get(j);
+                    Set<Integer> setB = setList.get(i - j);
+                    for (int a : setA) {
+                        for (int b : setB) {
+                            setList.get(i).add(a+b);
+                            setList.get(i).add(a-b);
+                            setList.get(i).add(a*b);
+                            if (b != 0)
+                                setList.get(i).add(a/b);
+                        }
+                    }
+                }
+            }
+            for (int i = 1; i <= 8; i++) {
+                for (int j : setList.get(i)) {
+                    if (j == number) {
+                        return i;
+                    }
+                }
+            }
+            return -1;
         }
     }
 
