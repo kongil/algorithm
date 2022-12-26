62b1ab07f42d433e5158e3043b6295033f4186a3
kongil <o_o0122@naver.com>
2022-12-27 00:08:14 +0900

    기능개발(Queue)

--- programmers2/src/StackQueue_기능개발.java
+++ programmers2/src/StackQueue_기능개발.java
@@ -6,9 +6,8 @@ public class StackQueue_기능개발 {
     static class Solution {
         public int afterOneDay(Queue<Integer> q, int[] speeds, int first) {
             int n = q.size();
-            for (int i = first; i < n; i++) {
+            for (int i = first; i < first + n; i++) {
                 int front = q.poll();
-                System.out.println(front);
                 q.add(front + speeds[i]);
             }
             int cnt = 0;
@@ -21,21 +20,29 @@ public class StackQueue_기능개발 {
         public int[] solution(int[] progresses, int[] speeds) {
             int n = progresses.length;
             Queue<Integer> q = new LinkedList<>();
-            int[] answer = new int[n];
+            Queue<Integer> answer = new LinkedList<>();
+
             int sum = 0;
             for (int i = 0; i < n; i++) {
                 q.add(progresses[i]);
             }
-            int aCnt = 0;
+
             while (!q.isEmpty()) {
-                System.out.println(q.size());
                 int cnt = afterOneDay(q, speeds, sum);
-                System.out.println("cnt : " + cnt);
-                answer[aCnt++] = cnt;
-                sum += cnt;
+                System.out.println("sum : " + sum + "cnt : " + cnt);
+                if (cnt > 0) {
+                    answer.add(cnt);
+                    sum += cnt;
+                }
             }
 
-            return answer;
+            int[] a = new int[answer.size()];
+            int cnt = 0;
+            while (!answer.isEmpty()) {
+                a[cnt++] = answer.poll();
+            }
+
+            return a;
         }
     }
     public static void main(String[] args) {
