07505cf6919322a54f6b7bdd5eb942b10b2a01ba
kongil <o_o0122@naver.com>
2022-12-28 00:05:30 +0900

    프린터(우선순위큐, 큐)

--- programmers2/src/StackQueue_프린터.java
+++ programmers2/src/StackQueue_프린터.java
@@ -1,11 +1,44 @@
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class StackQueue_프린터 {
     static class Solution {
+        class Print {
+            int i;
+            int priority;
+
+            public Print(int i, int priority) {
+                this.i = i;
+                this.priority = priority;
+            }
+        }
         public int solution(int[] priorities, int location) {
-            int answer = 0;
-            return answer;
+            PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+            Queue<Print> q = new LinkedList<>();
+            for (int i = 0; i < priorities.length; i++) {
+                int priority = priorities[i];
+
+                pq.add(priority);
+                q.add(new Print(i, priority));
+            }
+
+            int cnt = 0;
+            while (!pq.isEmpty()) {
+                if (q.peek().priority >= pq.peek()) {
+                    cnt++;
+                    if (q.peek().i == location) {
+                        return cnt;
+                    }
+                    q.poll();
+                    pq.poll();
+                }
+                else {
+                    q.add(q.poll());
+                }
+            }
+            return -1;
         }
     }
     public static void main(String[] args) {
