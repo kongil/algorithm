553c8691b883b6c7bf1ef351b5fe55d2c96e28ec
kongil <o_o0122@naver.com>
2022-12-27 23:54:28 +0900

    프린터

---
+++ programmers2/src/StackQueue_프린터.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StackQueue_프린터 {
+    static class Solution {
+        public int solution(int[] priorities, int location) {
+            int answer = 0;
+            return answer;
+        }
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] priorities = {2, 1, 3, 2};
+        int location = 2;
+        int answer = solution.solution(priorities, location);
+        System.out.println(answer);
+    }
+}
