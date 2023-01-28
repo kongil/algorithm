b7c44c2a09f407b08da408ecc3691ccace464420
kongil <o_o0122@naver.com>
2023-01-28 23:56:58 +0900

    N으로 표현

---
+++ programmers2/src/Dp_N으로_표현.java
@@ -0,0 +1,15 @@
+public class Dp_N으로_표현 {
+    static class Solution {
+        public int solution(int N, int number) {
+            int answer = 0;
+            return answer;
+        }
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int N = 5;
+        int number = 12;
+        int answer = solution.solution(N, number);
+        System.out.println("answer = " + answer);
+    }
+}
