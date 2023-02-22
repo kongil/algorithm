3e9c8e2c32f72470ff1735d6cbfcafced7fb3cc3
kongil <o_o0122@naver.com>
2023-02-22 23:54:06 +0900

    순위(그래프)

---
+++ programmers2/src/Graph_순위.java
@@ -0,0 +1,17 @@
+
+class Graph_순위{
+    static class Solution {
+        public int solution(int n, int[][] results) {
+            int answer = 0;
+            return answer;
+        }
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int n = 5;
+        int[][] edge = {{4, 3}, {4, 2}, {3, 2}, {1, 2}, {2, 5}};
+
+        System.out.println(solution.solution(n, edge));
+    }
+}
